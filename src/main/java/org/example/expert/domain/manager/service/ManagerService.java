66f0b8d7afdedd3e781046338cf49196d1b5452f
thezz9 <thezz9@naver.com>
2025-04-17 17:20:12 +0900

    Refactor: 레벨 3-2 date: 2025/04/17
    1. manager_목록_조회_시_Todo가_없다면_NPE_에러를_던진다() 수정
    2. comment_등록_중_할일을_찾지_못해_에러가_발생한다() 수정
    3. todo의_user가_null인_경우_예외가_발생한다() 서비스 로직 수정

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -35,7 +35,8 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
+        User writer = todo.getUser();
+        if (writer == null || !ObjectUtils.nullSafeEquals(user.getId(), writer.getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
 
